c62569f8547f87f45c65368ebc2c3c13a7495a04
RumenS <49361738+RumenStoev@users.noreply.github.com>
2020-01-27 21:27:00 +0200

    Create test.js

---
+++ Unit Testing and Modules/Simple test/test.js
@@ -0,0 +1,16 @@
+const {expect} = require("chai"); // Object
+const {solve,app,sum} = require("./index")
+
+
+describe("Test",() => {
+    it("Should be return String",function () {
+        expect(typeof sum).to.equal("function")
+    });
+    it('should be return String', function () {
+        let str = "Hello World!";
+        expect(solve()).to.be.equal(str)
+    });
+    it('should be return number ', function () {
+        expect(app()).to.be.equal(10)
+    });
+})
