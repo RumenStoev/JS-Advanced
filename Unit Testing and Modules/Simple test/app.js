d377a6b47fc8f156f6e302bdd26297bbeb2b29aa
RumenS <49361738+RumenStoev@users.noreply.github.com>
2020-01-27 21:27:35 +0200

    Create app.js

---
+++ Unit Testing and Modules/Simple test/app.js
@@ -0,0 +1,14 @@
+function solve() {
+    return "Hello World!"
+}
+
+function app() {
+    return 10;
+}
+function sum() {
+    return "Function from JavaScript!"
+}
+
+
+
+module.exports = {solve,app,sum}
