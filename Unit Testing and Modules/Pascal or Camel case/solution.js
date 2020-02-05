de72ced9716123f2f98e5462bc7e095b7b404098
RumenS <49361738+RumenStoev@users.noreply.github.com>
2020-02-05 23:14:06 +0200

    Update solution.js

--- Unit Testing and Modules/Pascal or Camel case/solution.js
+++ Unit Testing and Modules/Pascal or Camel case/solution.js
@@ -21,3 +21,34 @@ let types = {
         return str
     }
 }
+
+
+
+function tranformString(tranformation,str) {
+   return `${str[0][tranformation]() + str.slice(1,str.length)}`
+}
+
+const upperFirstLetter = tranformString.bind(undefined,"toLocaleUpperCase");
+const lowerFirstLetter = tranformString.bind(undefined,"toLocaleLowerCase");
+const reduceString = string => string
+    .toLocaleLowerCase()
+    .split(" ")
+    .map(upperFirstLetter)
+    .join("")
+
+
+let types = {
+    "Pascal Case" : reduceString,
+    "Camel Case" : string => lowerFirstLetter(reduceString(string))
+}
+
+function solve(type,string) {
+    if(typeof  types[type] === "undefined") {
+        return "Error!"
+    }
+    return types[type](string)
+}
+
+
+
+module.exports = solve;
