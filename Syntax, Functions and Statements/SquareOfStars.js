74bad27a94af966ae886701b4af48a3e5070a666
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-07-14 23:29:06 +0300

    Create SquareOfStars.js

---
+++ Syntax, Functions and Statements/SquareOfStars.js
@@ -0,0 +1,16 @@
+function SquareOfStars(num) {
+  for(let i = 0; i< num; i++) {
+    console.log("* ".repeat(num))
+  }
+}
+
+
+console.log(
+    SquareOfStars(5)
+)
+
+->  * * * * * 
+    * * * * * 
+    * * * * * 
+    * * * * * 
+    * * * * *
