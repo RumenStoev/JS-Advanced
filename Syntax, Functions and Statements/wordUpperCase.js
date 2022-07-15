51a8d8ba938f1448eeacf18b67984738ed8e83ad
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-07-15 16:43:18 +0300

    Create wordUpperCase.js

---
+++ Syntax, Functions and Statements/wordUpperCase.js
@@ -0,0 +1,24 @@
+function wordUpperCase(str) {
+
+    let pattern = /\w+/g;
+    let result = str.match(pattern)
+
+    return result.join(", ").toUpperCase()
+     
+}
+
+console.log(
+    wordUpperCase('Hi, how are you?')
+)
+console.log(
+    wordUpperCase("hello")
+)
+
+
+console.log(
+    wordUpperCase("Functions in JS can be nested, i.e. hold other functions")
+)
+
+->  HI, HOW, ARE, YOU
+    HELLO
+    FUNCTIONS, IN, JS, CAN, BE, NESTED, I, E, HOLD, OTHER, FUNCTIONS
