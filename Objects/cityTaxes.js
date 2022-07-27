e3f3fcd9fcc4b0d29462951ba3b992253409f2a0
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-07-27 18:28:06 +0300

    Create cityTaxes.js

---
+++ Objects/cityTaxes.js
@@ -0,0 +1,32 @@
+function cityTaxes(town, population, tresury) {
+    return {
+        name: town,
+        population: population,
+        treasury: tresury,
+        collectTaxes: function () {
+            this.treasury += this.population * this.taxRate
+        },
+        applyGrowth: function (percentage) {
+            this.population += (this.population * percentage) / 100
+        },
+        applyRecession: function (percentage) {
+            this.treasury -= (this.treasury * percentage) / 100
+        },
+        taxRate: 10
+
+    }
+
+}
+
+
+const city =
+    cityTaxes('Tortuga',
+        7000,
+        15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
+
+=> 85000
+   7350
