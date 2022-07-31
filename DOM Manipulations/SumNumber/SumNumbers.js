1335c7abeeccfe4d0716fd2a55b8ae3ca1a2f5aa
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-07-31 14:33:45 +0300

    Create SumNumbers.js

---
+++ DOM Manipulations/SumNumber/SumNumbers.js
@@ -0,0 +1,10 @@
+function calc() {
+  
+   let input1 = document.getElementById("num1");
+   
+   let input2 = document.getElementById("num2");
+   
+   let sum = document.getElementById("sum")
+
+   sum.value = Number(input1.value) + Number(input2.value);
+}
