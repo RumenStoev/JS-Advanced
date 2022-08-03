f3646616a8358c80a304a9a88699808ffc7b777b
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-08-03 12:28:04 +0300

    Create search.js

---
+++ DOM Manipulations/Search List/search.js
@@ -0,0 +1,27 @@
+//???
+function search() {
+  let listItems = document.getElementById("towns").children
+  let searchField = document.getElementById("searchText").value
+  let result = document.getElementById("result")
+  let counter = 0;
+   
+  let isMatched = (a,input) => input.split("").every((x,i) => x === a.textContent.split("")[i]) 
+ 
+  let changeState = a => {
+   a.style.textDecoration = " underline"
+   a.style.fontWeight = `bold`
+   counter = searchField.length
+    
+  }
+  let previousState = a =>  {
+    a.style.textDecoration = "" 
+    a.style.fontWeight = ""
+  }
+ 
+  Array.from(listItems).filter(x => (isMatched(x,searchField) && searchField.length !== 0) ?  changeState(x) : previousState(x))
+  
+  result.textContent = `${counter} matches found.`
+ 
+ }
+ 
+
