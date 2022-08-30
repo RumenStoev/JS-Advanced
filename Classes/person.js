90c7b4d4a8106e5c4022662b7c46e08751f2d6d1
RumenS <49361738+RumenStoev@users.noreply.github.com>
2022-08-30 15:54:10 +0300

    Create person.js

---
+++ Classes/person.js
@@ -0,0 +1,20 @@
+class Person {
+    constructor(firstName,lastName,age,email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email
+    }
+
+    toString() {
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+    }
+}
+
+
+let person = new Person("Peter", "Marinov", 18, "pesho18@abv.bg")
+console.log(person.toString());
+
+
+
+=>  Peter Marinov (age: 18, email: pesho18@abv.bg)
